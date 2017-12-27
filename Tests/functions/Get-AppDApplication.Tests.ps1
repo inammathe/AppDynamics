7707c347ecc99050a21d71270ce752b1617b8c57
evan.lock <evan.lock@tattsgroup.com>
2017-12-27 15:10:42 +1000

    Added Get-AppDApplication novalue test

--- Tests/functions/Get-AppDApplication.Tests.ps1
+++ Tests/functions/Get-AppDApplication.Tests.ps1
@@ -7,32 +7,42 @@ Import-Module "$moduleLocation\$module.psd1"
 InModuleScope $module {
     $function = 'Get-AppDApplication'
     Describe "$function Unit Tests" -Tag 'Unit' {
-        Context "$function return value validation" {
+        Context "$function return value validation. No Id. No Name." {
             $env:AppDURL = 'mockURL'
             $env:AppDAuth = 'mockAuth'
-            $env:AppDAccountID = $null
+            $env:AppDAccountID = 'mockAccountID'
 
-            <#
-            Mock Invoke-RestMethod -MockWith {
-                $mockData = @"
-"@
-                return [System.Management.Automation.PSSerializer]::DeserializeAsList($mockData)
-            }
+            Mock Get-AppDResource -MockWith {
+                $mockData = Import-CliXML -Path "$mockDataLocation\Get-AppDApplication.Mock"
+                return $mockData
+            } -ParameterFilter {$AppId -eq $null -and $AppName -eq $null}
 
-            $AccountId = Get-AppDApplication
+            Mock Get-AppDResource -MockWith {
+                $mockData = Import-CliXML -Path "$mockDataLocation\Get-AppDApplication.Mock"
+                return $mockData
+            } -ParameterFilter {$AppId -eq '6' -and $AppName -eq $null}
 
-            It "$function returns an id that is not null or empty" {
-                $AccountId | Should -not -BeNullOrEmpty
+            Mock Get-AppDResource -MockWith {
+                $mockData = Import-CliXML -Path "$mockDataLocation\Get-AppDApplication.Mock"
+                return $mockData
+            } -ParameterFilter {$AppId -eq '9' -and $AppName -eq $null}
+
+            Mock Get-AppDResource -MockWith {
+                $mockData = Import-CliXML -Path "$mockDataLocation\Get-AppDApplication.Mock"
+                return $mockData
+            } -ParameterFilter {$AppId -eq '11' -and $AppName -eq $null}
+
+            $ApplicationData = Get-AppDApplication
+
+            It "$function returns data that is not null or empty" {
+                $ApplicationData | Should -not -BeNullOrEmpty
             }
-            It "$function returns an id that is a string" {
-                $AccountId -is [string] | Should -Be $true
-            }
-            It "$function returns an id that is greater than 0" {
-                [int]$AccountId -ge 0 | Should -Be $true
+            It "$function returns all aplications" {
+                $ApplicationData.count -eq 3 | Should -Be $true
             }
             It "$function calls invoke-restmethod and is only invoked once" {
-                Assert-MockCalled -CommandName Invoke-RestMethod -Times 1 -Exactly
-            }#>
+                Assert-MockCalled -CommandName Get-AppDResource -Times 4 -Exactly
+            }
         }
     }
 }
